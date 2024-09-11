d2a405b3d74ce93d5acf564a9103bdb7ff97ed33
Faiznhmd <fiaznhmd.0624@gmail.com>
2024-09-11 20:42:20 +0530

    routes complete

---
+++ backend/model/authController.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+const userSchema = mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timeStamps: true,
+  }
+);
+
+export const User = mongoose.model('User', userSchema);
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,20 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import authRoute from './routes/authRoute.js';
+import postRoute from './routes/postRoute.js';
+
 import { connectDB } from './db/connectDB.js';
 
 const app = express();
 dotenv.config();
 const PORT = 8000;
+
+app.get('/', (req, res) => {
+  res.send('Hello world');
+});
+
+app.use('/api/auth', authRoute);
+app.use('/api/posts', postRoute);
 
 app.listen(PORT, () => {
   connectDB();
